d37f56919270942284ad8cd7cb1cc52e91368ea8
Tscheppi1008 <120476704+Tscheppi1008@users.noreply.github.com>
2023-02-17 13:22:14 +0100

    Add files via upload

---
+++ AoC2/AoC2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+List<int> numbersList = new List<int>();
+int currentNumber = 0;
+
+
+using (StreamReader reader = new StreamReader("Text.txt"))
+{
+    string line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (int.TryParse(line, out int parsedNumber))
+        {
+            // Wenn eine Zahl gelesen wurde, füge sie zur aktuellen Summe hinzu
+            currentNumber += parsedNumber;
+        }
+        else if (line.Trim() == "")
+        {
+            numbersList.Add(currentNumber);
+            currentNumber = 0;
+        }
+    }
+    if (((line = reader.ReadLine()) == null))
+    {
+        numbersList.Add(currentNumber);
+        currentNumber = 0;
+    }
+
+    for (int i = 0; i < numbersList.Count(); i++)
+    {
+        Console.WriteLine(numbersList[i]);
+    }
+    
+    Console.WriteLine();
+
+    numbersList.Sort();
+    numbersList.Reverse();
+
+    for (int i = 0; i < numbersList.Count(); i++)
+    {
+        Console.WriteLine(numbersList[i]);
+    }
+
+    Console.WriteLine();
+    int gesamtcal = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        gesamtcal += numbersList[i];
+    }
+    Console.WriteLine(gesamtcal);
+};
+
+
