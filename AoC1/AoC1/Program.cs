c9f46b9507c087efb099071543eb60e7bfed88a4
Tscheppi1008 <120476704+Tscheppi1008@users.noreply.github.com>
2023-02-17 11:08:21 +0100

    Add files via upload

---
+++ AoC1/AoC1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+List<int> numbersList = new List<int>();
+int currentNumber = 0;
+
+
+using (StreamReader reader = new StreamReader("Text.txt"))
+{
+    string line;
+
+    while ((line = reader.ReadLine()) != null)
+    {
+        if (int.TryParse(line, out int parsedNumber))
+        {
+            
+            currentNumber += parsedNumber;
+        }
+        else if (line.Trim() == "")
+        {
+            numbersList.Add(currentNumber);
+            currentNumber = 0;
+        }
+    }
+    if(((line = reader.ReadLine()) == null))
+    {
+        numbersList.Add(currentNumber);
+        currentNumber = 0;
+    }
+};
+
+
+Console.WriteLine(numbersList.Max());
+
